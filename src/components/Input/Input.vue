b6d715c5bc649c8edbffa4fbb35b3b300e739a1f
Bernardo <bernardoalvesbrum_100@hotmail.com>
2026-04-02 17:17:09 -0300

    feat: cart view form and validate

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -1,20 +1,28 @@
 <template>
-  <div class="cmp-input">
+  <div :class="['cmp-input', { invalid: isEmpty }]">
+    <label :for="inputName">{{ inputLabel }}</label>
     <input
+      :id="inputName"
       :name="inputName"
       :type="inputType"
       :placeholder="placeholder"
       :maxlength="maxlength"
       @input="$emit('update:modelValue', $event.target.value)"
       :value="modelValue"
+      v-maska="inputMask"
+      :required="required"
+      :autocomplete="autocomplete"
+      @blur="$emit('blur', $event.target.value)"
     />
+    <p class="error-message">Este campo não pode ser vazio!</p>
   </div>
 </template>
 
 <script setup>
 import "./Input.less";
+import { vMaska } from "maska/vue";
 
-const emit = defineEmits(["update:modelValue"]);
+const emit = defineEmits(["update:modelValue", "blur"]);
 
 defineProps({
   modelValue: {
@@ -37,6 +45,26 @@ defineProps({
   maxlength: {
     type: String,
     required: false,
+  },
+  inputMask: {
+    type: String,
+    required: false,
+  },
+  required: {
+    type: Boolean,
+    default: false,
+  },
+  inputLabel: {
+    type: String,
+    required: false,
+  },
+  autocomplete: {
+    type: String,
+    required: false,
+  },
+  isEmpty: {
+    type: Boolean,
+    default: false,
   },
 });
 </script>
--- src/views/CartView/CartView.vue
+++ src/views/CartView/CartView.vue
@@ -1,63 +1,143 @@
 <template>
   <div class="view cart-view">
     <div class="container-left">
+      <button>Continuar comprando</button>
       <h1>Finalização do pedido</h1>
-      <form>
+      <form @submit.prevent="submitForm">
         <div class="contact">
           <h2>Informações de contato</h2>
-          <label>
-            Email
-            <Input input-name="email" placeholder="seuemail@exemplo.com.br" />
-          </label>
-          <label>
-            Telefone
-            <Input input-name="phone" placeholder="(00) 00000-0000" />
-          </label>
+          <Input
+            input-type="email"
+            input-name="email"
+            placeholder="seuemail@exemplo.com.br"
+            :required="true"
+            input-label="E-mail"
+            maxlength="60"
+            autocomplete="email"
+            v-model="form.email"
+            @blur="handleBlur('email')"
+            :is-empty="emptyFields.email"
+          />
+          <Input
+            input-type="tel"
+            input-name="phone"
+            placeholder="(00) 00000-0000"
+            :required="true"
+            input-label="Telefone"
+            input-mask="(##) #####-####"
+            autocomplete="tel"
+            v-model="form.phone"
+            @blur="handleBlur('phone')"
+            :is-empty="emptyFields.phone"
+          />
         </div>
         <div class="delivery">
           <h2>Informações de entrega</h2>
-          <label>
-            CEP
-            <Input input-name="cep" placeholder="00000-000" />
-          </label>
-          <label>
-            Rua
-            <Input input-name="street" placeholder="Nome da rua" />
-          </label>
-          <label>
-            Cidade
-            <Input input-name="city" placeholder="Cidade" />
-          </label>
-          <label>
-            Bairro
-            <Input input-name="neighborhood" placeholder="Bairro" />
-          </label>
-          <label>
-            Estado
-            <Input input-name="state" placeholder="Estado" />
-          </label>
+          <Input
+            input-name="cep"
+            placeholder="00000-000"
+            :required="true"
+            input-label="CEP"
+            input-mask="#####-###"
+            autocomplete="postal-code"
+            v-model="form.cep"
+            @blur="handleBlur('cep')"
+            :is-empty="emptyFields.cep"
+          />
+          <Input
+            input-name="street"
+            placeholder="Nome da rua"
+            :required="true"
+            input-label="Rua"
+            maxlength="60"
+            autocomplete="address-line1"
+            v-model="form.street"
+            @blur="handleBlur('street')"
+            :is-empty="emptyFields.street"
+          />
+          <Input
+            input-name="city"
+            placeholder="Cidade"
+            :required="true"
+            input-label="Cidade"
+            maxlength="30"
+            autocomplete="address-level2"
+            v-model="form.city"
+            @blur="handleBlur('city')"
+            :is-empty="emptyFields.city"
+          />
+          <Input
+            input-name="neighborhood"
+            placeholder="Bairro"
+            :required="true"
+            input-label="Bairro"
+            maxlength="30"
+            autocomplete="address-level3"
+            v-model="form.neighborhood"
+            @blur="handleBlur('neighborhood')"
+            :is-empty="emptyFields.neighborhood"
+          />
+          <Input
+            input-name="state"
+            placeholder="Estado"
+            :required="true"
+            input-label="Estado"
+            maxlength="30"
+            autocomplete="address-level1"
+            v-model="form.state"
+            @blur="handleBlur('state')"
+            :is-empty="emptyFields.state"
+          />
         </div>
         <div class="payment">
           <h2>Informações de pagamento</h2>
-          <label>
-            Número do cartão
-            <Input input-name="cardNumber" placeholder="0000 0000 0000 0000" />
-          </label>
-          <label>
-            Titular do cartão
-            <Input input-name="cardHolder" placeholder="Nome completo" />
-          </label>
-          <label>
-            Data de validade
-            <Input input-name="cardExpiration" placeholder="MM/AA" />
-          </label>
-          <label>
-            CVC
-            <Input input-name="cardCVC" placeholder="000" />
-          </label>
+          <Input
+            input-name="cardNumber"
+            placeholder="0000 0000 0000 0000"
+            :required="true"
+            input-label="Número do cartão"
+            input-mask="#### #### #### ####"
+            autocomplete="cc-number"
+            v-model="form.cardNumber"
+            @blur="handleBlur('cardNumber')"
+            :is-empty="emptyFields.cardNumber"
+          />
+          <Input
+            input-name="cardHolder"
+            placeholder="Nome completo"
+            :required="true"
+            input-label="Titular do cartão"
+            maxlength="30"
+            autocomplete="cc-name"
+            v-model="form.cardHolder"
+            @blur="handleBlur('cardHolder')"
+            :is-empty="emptyFields.cardHolder"
+          />
+          <Input
+            input-name="cardExpiration"
+            placeholder="MM/AA"
+            :required="true"
+            input-label="Data de vencimento"
+            input-mask="##/##"
+            autocomplete="cc-exp"
+            v-model="form.cardExpiration"
+            @blur="handleBlur('cardExpiration')"
+            :is-empty="emptyFields.cardExpiration"
+          />
+          <Input
+            input-name="cardCVC"
+            placeholder="000"
+            :required="true"
+            input-label="CVC"
+            input-mask="###"
+            autocomplete="c-c-c-sc"
+            v-model="form.cardCVC"
+            @blur="handleBlur('cardCVC')"
+            :is-empty="emptyFields.cardCVC"
+          />
         </div>
         <div class="finish-button">
-          <button type="submit">Fechar pedido</button>
+          <button type="submit">Concluir pedido</button>
         </div>
       </form>
     </div>
@@ -103,14 +183,50 @@
 import "./CartView.less";
 import { Input } from "@/components";
 import { useStore } from "@/store";
-import { computed } from "vue";
+import { computed, reactive } from "vue";
 
 const { getShoppingCart, setQuantity } = useStore();
 const items = computed(() => getShoppingCart());
+
+const form = reactive({
+  email: "",
+  phone: "",
+  cep: "",
+  street: "",
+  city: "",
+  neighborhood: "",
+  state: "",
+  cardNumber: "",
+  cardHolder: "",
+  cardExpiration: "",
+  cardCVC: "",
+});
+
+const emptyFields = reactive({
+  email: false,
+  phone: false,
+  cep: false,
+  street: false,
+  city: false,
+  neighborhood: false,
+  state: false,
+  cardNumber: false,
+  cardHolder: false,
+  cardExpiration: false,
+  cardCVC: false,
+});
 
 const totalValue = computed(() => {
   return items.value.reduce((total, item) => {
     return total + item.price * item.quantity;
   }, 0);
 });
+
+const submitForm = () => {
+  console.log("Enviado");
+};
+
+const handleBlur = (field) => {
+  emptyFields[field] = !form[field];
+};
 </script>
